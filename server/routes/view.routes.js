8be90d910311fc209dfa8de0eda045aecb595e8d
dantheman <danogbo0@gmail.com>
2024-08-17 14:08:44 +0100

    feat: login and register

--- server/routes/view.routes.js
+++ server/routes/view.routes.js
@@ -2,7 +2,15 @@ import express from "express";
 const router = express.Router();
 
 router.get("/home", async function (req, res) {
-  res.render("home");
+  res.render("home", );
+});
+
+router.get("/register", async function (req, res) {
+  res.render("auth/register");
+});
+
+router.get("/login", async function (req, res) {
+  res.render("auth/login");
 });
 
 export default router;
