34f9190c47976184154f266b515cae792199e350
dantheman <danogbo0@gmail.com>
2024-08-19 20:39:52 +0100

    update: transactions complete

--- server/routes/view.routes.js
+++ server/routes/view.routes.js
@@ -31,4 +31,10 @@ router.get("/transactions", async function (req, res) {
   res.render("transactions/my_transactions");
 });
 
+
+// my store - view my store and items purchased
+router.get("/my-store", async function (req, res) {
+  res.render("transactions/my_store_transactions");
+});
+
 export default router;
