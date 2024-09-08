582280dd35d06d07e9ddfd67841eeca1efcb3644
Daniel-16 <danieloloruntoba681@gmail.com>
2024-09-08 13:46:36 +0100

    feat: randomize tokens to items

--- server/controllers/ItemsController.js
+++ server/controllers/ItemsController.js
@@ -1,5 +1,6 @@
 import ItemModel from "../models/ItemsModel.js";
 import StoreModel from "../models/StoreModel.js";
+import TokenModel from "../models/TokenModel.js";
 import UserModel from "../models/UserModel.js";
 
 export const createItem = async (req, res) => {
@@ -38,6 +39,31 @@ export const getAllItems = async (req, res) => {
     res.status(200).json({
       success: true,
       items,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      error: error.message,
+    });
+  }
+};
+
+export const attachTokenToItem = async (req, res) => {
+  try {
+    const items = await ItemModel.find({ attachedToken: null });
+    const tokens = await TokenModel.find({});
+
+    for (const item of items) {
+      if (Math.random() < 0.1) {
+        const randomToken = tokens[Math.floor(Math.random() * tokens.length)];
+        item.attachedToken = randomToken._id;
+        await item.save();
+      }
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "Tokens attached to items successfully",
     });
   } catch (error) {
     res.status(500).json({
