4e726a12148b26875dde43f57a3313dae12721d1
Daniel-16 <danieloloruntoba681@gmail.com>
2024-10-15 23:43:19 +0100

    fix: return store owner as well as coupons

--- server/controllers/MarketPlace.js
+++ server/controllers/MarketPlace.js
@@ -19,12 +19,21 @@ export const getAllCoupons = async (req, res) => {
 export const getStoreCoupons = async (req, res) => {
   const { accountAddress } = req.params;
   const userId = req.user.id;
+
   try {
-    const user = await UserModel.findById(userId);
-    if (!user) {
+    const loggedInUser = await UserModel.findById(userId);
+    if (!loggedInUser) {
+      return res.status(401).json({
+        success: false,
+        error: "User not authenticated",
+      });
+    }
+
+    const storeOwner = await UserModel.findOne({ accountAddress });
+    if (!storeOwner) {
       return res.status(404).json({
         success: false,
-        error: "User not found",
+        error: "Store owner not found",
       });
     }
     const coupons = await TokenModel.find({
@@ -32,6 +41,7 @@ export const getStoreCoupons = async (req, res) => {
     });
     res.status(200).json({
       success: true,
+      storeOwner,
       coupons,
     });
   } catch (error) {
