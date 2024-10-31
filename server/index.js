ea0cfee0b942c8c9d9fed6956530217b6ec1039e
Daniel-16 <danieloloruntoba681@gmail.com>
2024-10-31 10:01:50 +0100

    feat: log users that gains up to 100 points

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 import "./utils/scheduler.js";
 import "./utils/scheduleWinningUser.js";
+import "./utils/logHighPointUsers.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
